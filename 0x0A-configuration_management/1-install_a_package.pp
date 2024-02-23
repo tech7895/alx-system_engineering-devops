1841bb8a3657b0169f2cda1ec79b472be9fd3324
tech7895 <techhunter990@gmail.com>
2024-02-23 04:55:02 -0800

    t1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# puppet declarative script to install flask from pip3.
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'python3-pip':
+  ensure => installed,
 }
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install flask==2.1.0',
+  path        => ['/usr/bin'],
+  environment => ['LC_ALL=en_US.UTF-8', 'LANG=en_US.UTF-8'], # Adjust locale if needed
+  unless      => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"', # Ensure version 2.1.0 is installed
+}
+
